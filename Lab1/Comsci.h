0a0ef8ed75190114448534d96b8412d41a30f34e
10100programer <alexmettler@hotmail.com>
2017-01-29 14:33:27 -0600

    Pushing Changes for the day, created most functions needed for the first part of the assignment, right now everything except negative exponets works

---
+++ Lab1/Comsci.h
@@ -0,0 +1,73 @@
+﻿/*
+Comsci.h											         		
+Standard header to be included with programs created in Computer Science related classes
+Version	:1.2b
+Created	:April		25th 2015
+Modified:January	29th 2017
+*/
+
+//#include "stdafx.h"
+#include <iostream>
+#include "windows.h"
+#include <conio.h>
+#include <cstdlib>
+#include <string>
+#include <stdio.h>
+#include <tchar.h>
+#include <cmath>
+#include <fstream>
+#define spacer "|------------------------------------------------------------------------------|"
+
+int splash(std::string title = "Untitled", std::string purpose = "N/A") //splash screen for programs
+{
+	
+	//std::cout << "********************************************************************\n";
+	std::cout << spacer;
+	std::cout << " Using Comsci.h \n";
+	std::cout << " Program Name	: "<< title << std::endl;
+	std::cout << " Description	: " << purpose << std::endl;;
+	std::cout << spacer<<std::endl;
+	//std::cout << "********************************************************************\n";
+		return 0;
+}
+
+void pause(std::string word = "Press any key to continue\n") //easy pause command
+{
+	std::cout << word;
+	std::cin.get();
+}
+int color(std::string doscolor) //Simple color handler
+{
+	std::string syscolor; // green, default, blue
+	{
+		if (doscolor == "green")
+			doscolor = "0A";
+		if (doscolor == "default")
+			doscolor = "07";
+		if (doscolor == "blue")
+			doscolor = "9E";
+		syscolor = "color " + doscolor;
+		system(syscolor.c_str());
+		return 3;
+	}
+}
+std::string centertext(std::string imput)
+{
+	std::string z2;
+	int math;
+	int screenwidth = 78;
+	math = (78 - imput.length()) / 2;
+	if (imput.length() > 80)
+		goto exit;
+	int counter = 0;
+	while (math > counter)
+	{
+		z2 = z2 + " ";
+		counter++;
+	}
+	z2 = z2 + imput;
+	exit:
+	return z2;
+
+}
+//EOF
--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -2,12 +2,38 @@
 //
 
 #include "stdafx.h"
-
-
+#include "Comsci.h"
+float power(float a, int b);// a is number and b is power of
+float flip(float a);
 int main()
 {
-	return 2;
-	return 1;
-    return 0;
+	int ans = 1;
+	//std::cout << power(5, -2) << std::endl;
+	while (ans != -9) {
+		std::cin >> ans;
+		std::cout << flip(ans);
+	}
+	pause(); //pauses execution of code to view answer and prints
+	return 0;
 }
-
+float power(float a, int b)
+{
+	
+	if (b < 0)//negative number handler
+	{
+		unsigned int  d = b;
+		d = b;
+		a = flip(a);
+		//pause();
+	}
+	if (a ==0)//nothing to compute
+		return 0;
+	if (b == 0) //anything to the power of 0 is zero except for zero
+		return 1;
+	else
+		return a *power(a,b-1);
+}
+float flip(float a)
+{
+	return 1 / a;
+}
