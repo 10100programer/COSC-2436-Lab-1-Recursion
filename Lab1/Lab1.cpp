71e59bb61c077ea41554182f40cdb235deee0548
10100programer <alexmettler@hotmail.com>
2017-01-31 14:40:40 -0600

    Added a menu for presenting

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -6,10 +6,10 @@
 float power(float a, int b);// a is number and b is power of
 float flip(float a);
 void lab15_1();
-
+void mainmenu();
 int main()
 {	
-	lab15_1();
+	mainmenu();
 	pause("Program execution Terminated press any key to exit");
 	return 0;
 }
@@ -28,6 +28,26 @@ float power(float a, int b)
 float flip(float a)
 {
 	return 1 / a;
+}
+void mainmenu()
+{
+	
+	while (true)
+	{
+		std::string name;
+		color("blue");
+		std::cout << centertext("***Lab 1:  Recursion***") << std::endl;
+		std::cout << centertext("***Please Make A Selction***") << std::endl;
+		std::getline(std::cin, name);
+		if (name == "a" || name == "A") 
+		{
+			lab15_1();
+		}
+		else if(true)
+		{
+			break;
+		}
+	}
 }
 void lab15_1()
 {
@@ -57,4 +77,13 @@ void lab15_1()
 	//std::cout << power(5, -2) << std::endl;
 	pause(); //pauses execution of code to view answer and prints
 	return;
+	/* 
+	Expected Output
+	Please Enter The Number you want to raise by a power 
+	2
+	Please Enter The Power you want to raise 2 by
+	16
+	The Answer of 2^16 is 65536
+	Press Any Key to try Again or -12 to exit
+	*/
 }
